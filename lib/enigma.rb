3d4bc49560c943825485d5d6338aed66ad227216
Judson Stevens <Judson.Stevens@gmail.com>
2018-05-10 18:46:56 -0600

    Working on character rotation, WIP

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -4,12 +4,70 @@ class Enigma
     @character_map = [*('a'..'z'), ' ', '.', ',']
   end
 
-  def encrypt(message, code, date)
-    
+  def encrypt(message, code = rand(10000..99999).to_s, date)
+
   end
 
   def strip_and_make_message_array(message)
     message.strip.chars
   end
 
+  def format_date_object(date)
+    date.strftime('%m%d%y')
+  end
+
+  def character_rotator(message)
+    message_array = strip_and_make_message_array(message)
+    message_array.map_with_index do |character, index|
+      until index == (message_array.length - 1)
+        message_array.take(4).map do |element|
+          
+          @character_map.index(element)
+        end
+      end
+    end
+  end
+
+  def find_the_rotated_index(character)
+    generate_character_rotation
+
+
+  def generate_character_rotation(code, date)
+    [generate_a_rotation(code, date),
+     generate_b_rotation(code, date),
+     generate_c_rotation(code, date),
+     generate_d_rotation(code, date)]
+  end
+
+  def generate_a_rotation(code, date)
+    code[0..1].to_i + generate_offset_a(date).to_i
+  end
+
+  def generate_b_rotation(code, date)
+    code[1..2].to_i + generate_offset_b(date).to_i
+  end
+
+  def generate_c_rotation(code, date)
+    code[2..3].to_i + generate_offset_c(date).to_i
+  end
+
+  def generate_d_rotation(code, date)
+    code[3..4].to_i + generate_offset_d(date).to_i
+  end
+
+  def generate_offset_a(date)
+    (date.to_i ** 2).to_s[-4]
+  end
+
+  def generate_offset_b(date)
+    (date.to_i ** 2).to_s[-3]
+  end
+
+  def generate_offset_c(date)
+    (date.to_i ** 2).to_s[-2]
+  end
+
+  def generate_offset_d(date)
+    (date.to_i ** 2).to_s[-1]
+  end
 end
--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -2,20 +2,59 @@ require 'time'
 require_relative '../lib/enigma.rb'
 
 RSpec.describe Enigma do
+  before{(@enigma_machine = Enigma.new)}
   describe 'encryption of message' do
     it 'can encrypt a message' do
-      enigma_machine = Enigma.new
       message = 'Hello'
-      code = 123412
+      code = '123412'
       date = Date.today
 
-      expect(enigma_machine.encrypt(message, code, date)).to eq('')
+      expect(@enigma_machine.encrypt(message, code, date)).to eq('')
     end
 
     it 'can generate the correct character map' do
-      enigma_machine = Enigma.new
-      
-      expect(enigma_machine.character_map[4]).to eq('e')
+      expect(@enigma_machine.character_map[4]).to eq('e')
+    end
+
+    it 'can rotate characters using the character map' do
+      expect(@enigma_machine.character_rotator('Hello')).to eq('')
+    end
+    
+
+    it 'can generate the correct rotation with a date and code' do
+      expect(@enigma_machine.generate_character_rotation('12341', "051018").join).to eq('18263645')
+    end
+
+    it 'can generate the correct A rotation' do
+      expect(@enigma_machine.generate_a_rotation('12341', "051018")).to eq(18)
+    end
+
+    it 'can generate the correct B rotation' do
+      expect(@enigma_machine.generate_b_rotation('12341', "051018")).to eq(26)
+    end
+
+    it 'can generate the correct A rotation' do
+      expect(@enigma_machine.generate_c_rotation('12341', "051018")).to eq(36)
+    end
+
+    it 'can generate the correct A rotation' do
+      expect(@enigma_machine.generate_d_rotation('12341', "051018")).to eq(45)
+    end
+
+    it 'can generate the correct A offset' do
+      expect(@enigma_machine.generate_offset_a("051018")).to eq('6')
+    end
+
+    it 'can generate the correct B offset' do
+      expect(@enigma_machine.generate_offset_b("051018")).to eq('3')
+    end
+
+    it 'can generate the correct C offset' do
+      expect(@enigma_machine.generate_offset_c("051018")).to eq('2')
+    end
+
+    it 'can generate the correct D offset' do
+      expect(@enigma_machine.generate_offset_d("051018")).to eq('4')
     end
 
   end
